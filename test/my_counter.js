09fcdc7e04f9f27795fe04595c16bb0b0d12766c
yoshiioko <acardoza@pdx.edu>
2022-06-30 09:14:51 -0700

    Update: Scaffold unit test file

--- test/my_counter.js
+++ test/my_counter.js
@@ -1,13 +1,23 @@
 const MyCounter = artifacts.require("MyCounter");
 
+const { BN } = require("@openzeppelin/test-helpers");
+
+const { expect } = require("chai");
+
 /*
  * uncomment accounts to access the test accounts made available by the
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("MyCounter", function (/* accounts */) {
-  it("should assert true", async function () {
-    await MyCounter.deployed();
-    return assert.isTrue(true);
+contract("MyCounter", function (accounts) {
+  const [owner] = accounts;
+
+  beforeEach(async function () {
+    this.contract = await MyCounter.new({ from: owner });
+  });
+
+  it("Verify contract initialized", async function () {
+    let initialValue = new BN(0);
+    expect(await this.contract.getCount()).to.be.bignumber.equal(initialValue);
   });
 });
