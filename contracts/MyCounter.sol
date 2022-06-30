d6d4726958ab416b6a9f3113287a07a17bc196f8
yoshiioko <acardoza@pdx.edu>
2022-06-30 08:42:52 -0700

    Update: Document the contract and its functions.

--- contracts/MyCounter.sol
+++ contracts/MyCounter.sol
@@ -1,21 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.13 <0.9.0;
 
+/**
+ * @dev Contract implements a counter that can be incremented and decremented by users.
+ */
 contract MyCounter {
   uint count;
 
+    /**
+     * @dev Initializes the contract setting the count to an initial value.
+     */
   constructor(uint initialCount) {
     count = initialCount;
   }
 
+    /**
+     * @dev Returns the current value of count.
+     */
   function getCount() external view returns (uint) {
     return count;
   }
 
+    /**
+     * @dev Increases the count by 1.
+     */
   function increment() external {
     count += 1;
   }
 
+    /**
+     * @dev Decreases the count by 1.
+     */
   function decrement() external {
     count -= 1;
   }
